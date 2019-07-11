acad9dcc47399708915e3635df6cca7fbcfcd360
Nelly Moseki <nelly.m@afrihost.com>
2019-07-11 15:18:59 +0200

    CT: Add removed js file plugins

--- themes/croptop/src/js/index.js
+++ themes/croptop/src/js/index.js
@@ -1,18 +1,12 @@
 /*
-
 -  index.js file will serve as a file to create all jquery animations
 - Plugins used are:
 -- jqFloat.js
--- 
-
+-- owlCarousel.js
 */
-
-// Make footer elephant image float
 $(function(){
-	$('.big-elephant').jqFloat(
 
-		width: 300,
-        height: 300,
-        speed: 100
-	);
+
 });
+
+
