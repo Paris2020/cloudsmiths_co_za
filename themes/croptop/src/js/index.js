c34f6182663d9d1e819749a6730906850ec92051
Nelly Moseki <nelly.m@afrihost.com>
2019-07-15 15:49:30 +0200

    CT: Troubleshooting bounce effect

--- themes/croptop/src/js/index.js
+++ themes/croptop/src/js/index.js
@@ -5,10 +5,23 @@
 -- owlCarousel.js
 -- Lettering.js
 */
+
 $(function(){
 
-	$(".section-title").lettering();
+  
+  $(".section-title").lettering();
+  /* 
+  - Onload bounce the h1 section letters
+  */
+
+    $('.section-title').effect("bounce", {
+      times: 4,
+      distance: 200
+    }, 400).click(function() {
+      $(this).effect("bounce", {
+        times: 4,
+        distance: 200
+      }, 400);
+    });
 
 });
-
-
