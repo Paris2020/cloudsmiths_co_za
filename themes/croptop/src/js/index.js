5725c54db12ae232b7d82a0d3b66cb705984dc5e
Nelly Moseki <nelly.m@afrihost.com>
2019-07-16 13:06:32 +0200

    CT: Modified index.js file

--- themes/croptop/src/js/index.js
+++ themes/croptop/src/js/index.js
@@ -6,22 +6,38 @@
 -- Lettering.js
 */
 
-$(function(){
+(function ($) {
 
-  
   $(".section-title").lettering();
-  /* 
-  - Onload bounce the h1 section letters
-  */
 
-    /*$('.section-title').effect("bounce", {
+  /*
+    - Make the title character bounce onload of the page
+  */
+  $('.section-title').effect("bounce", {
+    times: 4,
+    distance: 200
+  }, 400).click(function() {
+    $(this).effect("bounce", {
       times: 4,
       distance: 200
-    }, 400).click(function() {
-      $(this).effect("bounce", {
-        times: 4,
-        distance: 200
-      }, 400);
-    });*/
+    }, 400);
+  });
+
+  /*
+    - Slow scroll to Anchors
+  */
+  $('a[href^="#"]').on('click', function(e){
+
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      jQuery('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
 
 });
